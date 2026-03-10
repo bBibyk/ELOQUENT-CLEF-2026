95d9620dafe03b460edaeb616cf4b9fbf007cf70
Bogdan Bibyk <bbibyk@gmail.com>
2026-03-10 16:38:20 +0100

    Experiment minimum version

--- src/experiment.py
+++ src/experiment.py
@@ -1,14 +1,24 @@
 import enums
-from models import *
+from models import abstract_model
+
+import time
+import random
+import string
 
 class Experiment:
-    def __init__(self, model : enums.Model, languages : set[enums.LanguageCode], do_sample : bool = False, temprature : int = 0,
+    def __init__(self, model_choice : enums.Model, languages : set[enums.LanguageCode], do_sample : bool = False, temprature : int = 0,
                  system_promp : str = "", prefix : str = "", suffix : str = "", title : str = ""):
-        self._model : abstract_model.AbstractModel = model
+        self._model : abstract_model.AbstractModel = model_choice.to_model()
 
-    def run():
-
-
+    def run(self):
+        """
+        Générateur qui produit les résultats intermédiaires
+        """
+        for i in range(1, 11):
+            random_str = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
+            result = f"Pass {i}/10 - Modèle: {self.title} - Data: {random_str}"
+            yield result
+            time.sleep(1)
 
 
 
